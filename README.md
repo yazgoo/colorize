446e139f36228f61351560deb3a3020de03224fb
yazgoo <yazgoo@github.com>
2014-09-03 07:47:53 +0200

    adding backup, cleanup

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ It allows to highligt parts of the text via pre-defined styles.
 Just click on the style you want to use, then select the text you want to highlight.
 For each file name, all your changes are stored on localStorage.
 You can clear this storage using "clear".
+If you want to backup your work, use the "backup hexa" button.
+You can then load it via "load hexa", which will overwrite your localStorage copy.
 
 Customizing
 ===========
@@ -15,3 +17,9 @@ Customizing
 The pre-defined CSS styles are defined both
   *  in colors.css: the styles
   *  in colors.js: their description
+
+
+Limitations
+==========
+
+Multi-line selections are not yet supported.
--- colorize.js
+++ colorize.js
@@ -1,3 +1,7 @@
+function update_storage()
+{
+    localStorage.setItem(document.getElementById('fileInput').value, document.getElementById('bytes').innerHTML);
+}
 function highlight()
 {
     var selection = window.getSelection();
@@ -9,14 +13,23 @@ function highlight()
     node.setAttribute("class", document.getElementById('current').className)
     range.insertNode(node);
     selection.removeAllRanges();
-    localStorage.setItem(document.getElementById('fileInput').value, document.getElementById('bytes').innerHTML);
+    update_storage();
 }
 function clear_bytes()
 {
     localStorage.removeItem(document.getElementById('fileInput').value);
     document.getElementById('bytes').innerHTML = ""
 }
-
+function save_file()
+{
+    var a = window.document.createElement('a');
+    var content = document.getElementById('bytes').innerHTML;
+    a.href = window.URL.createObjectURL(new Blob([content], {type: 'text/html'}));
+    a.download = document.getElementById('fileInput').value + '.html';
+    document.body.appendChild(a)
+        a.click();
+    document.body.removeChild(a)
+}
 function select_class(_style) 
 {
     document.getElementById('current').className = _style;
@@ -31,28 +44,33 @@ function fill_caption()
 }
 function setup_file_input()
 {
+    var addressesDiv = document.getElementById('addresses');
+    var shortsDiv = document.getElementById('shorts');
+    var floatsDiv = document.getElementById('floats');
+    var bytesDiv = document.getElementById('bytes');
+    var charsDiv = document.getElementById('chars');
     var fileInput = document.getElementById('fileInput');
     fileInput.addEventListener('change', function(e) {
             var file = fileInput.files[0];
             var reader = new FileReader();
             reader.onload = function(e) {
               var shorts = new Int16Array(reader.result);
-              var addressesDiv = document.getElementById('addresses');
-              var shortsDiv = document.getElementById('shorts');
-              var floatsDiv = document.getElementById('floats');
-              var bytesDiv = document.getElementById('bytes');
-              var charsDiv = document.getElementById('chars');
 
+              shortsDiv.innerHTML = ""
+              addressesDiv.innerHTML = ""
               for(i in shorts) {
                   if(i % 8 == 0) addressesDiv.innerHTML += i.toString(16) + "<br/>";
                   shortsDiv.innerHTML += shorts[i] + " ";
                   if(i % 8 == 7) shortsDiv.innerHTML += "<br/>"
               }
+              floatsDiv.innerHTML = ""
               var floats = new Float32Array(reader.result);
               for(i in floats) {
                   floatsDiv.innerHTML += floats[i] + " ";
                   if(i % 4 == 3) floatsDiv.innerHTML += "<br/>"
               }
+              charsDiv.innerHTML = ""
+              bytesDiv.innerHTML = ""
               var bytes = new Uint8Array(reader.result);
               for(i in bytes) {
                   var b = bytes[i].toString(16)
@@ -66,6 +84,10 @@ function setup_file_input()
                       bytesDiv.innerHTML += "<br/>"
                       charsDiv.innerHTML += "<br/>"
                   }
+                  else if(i % 4 == 3)
+                  {
+                      bytesDiv.innerHTML += " "
+                  }
               }
               document.title = "Colorize - " + fileInput.value
               var item = localStorage.getItem(fileInput.value);
@@ -74,4 +96,14 @@ function setup_file_input()
               }
             reader.readAsArrayBuffer(file);    
             });
+    var hexaFileInput = document.getElementById('hexaFileInput');
+    hexaFileInput.addEventListener('change', function(e) {
+            var file = hexaFileInput.files[0];
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                bytesDiv.innerHTML = reader.result;
+                update_storage();
+            }
+            reader.readAsText(file);
+    });
 }
